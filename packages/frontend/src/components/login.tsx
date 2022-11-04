fafcd706b5ea97b5e4001bc22e7a46307ebca95b
Gabi Dombrowski <gabdombrowski@gmail.com>
2022-11-04 14:11:29 -0500

    Add navigation on authed

--- packages/frontend/src/components/login.tsx
+++ packages/frontend/src/components/login.tsx
@@ -1,14 +1,18 @@
-import { useNavigate } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import { Navigate, useNavigate } from "react-router-dom";
 import "../App.css";
 
 export function Login() {
   const navigate = useNavigate();
+  const { authState } = useOktaAuth();
 
   const handleLoginClick = () => {
     navigate("/facilities");
   };
 
-  return (
+  return authState?.isAuthenticated ? (
+    <Navigate to="/facilities" replace />
+  ) : (
     <div className="form-wrapper">
       <form onSubmit={handleLoginClick}>
         <h2>Welcome Back!</h2>
